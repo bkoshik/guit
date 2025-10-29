cf7b42144e1ae2ebf2fb8ad23e5dc1a8026a8c8c
bkoshik <bakubakosh@gmail.com>
2025-10-29 12:09:47 +0500

    Add consts

---
+++ src/project/consts.rs
@@ -0,0 +1,14 @@
+pub const DEFAULT_REPO_PATH: &str = ".";
+pub const NO_MESSAGE: &str = "\x1b[2;3mNo message\x1b[0m";
+pub const NO_NAME: &str = "\x1b[2;3mNo name\x1b[0m";
+pub const NO_EMAIL: &str = "\x1b[2;3mNo email\x1b[0m";
+
+
+// COLORS
+
+pub const LIGHT_BLUE: &str = "\x1b[36m";
+pub const YELLOW: &str = "\x1b[33m";
+pub const GREEN: &str = "\x1b[32m";
+pub const RED: &str = "\x1b[31m";
+
+pub const CLEAR: &str = "\x1b[0m";
